3f37ce51da3f731ab6bfb9c073b1ae607b00342a
beregrande <60332084+beregrande@users.noreply.github.com>
2020-02-26 14:53:32 -0600

    homework1

--- solver.py
+++ solver.py
@@ -4,6 +4,9 @@
 from collections import namedtuple
 Item = namedtuple("Item", 'index weight value')
 
+import itertools
+import functools
+
 def trivial_greedy(items, capacity):
     # a trivial greedy algorithm for filling the knapsack
     # it takes items in-order until the knapsack is full
@@ -24,26 +27,82 @@ def trivial_greedy(items, capacity):
 
 ## TODO: ¡¡¡AGREGA  AQUÍ TUS FUNCIONES!!!
 
-#Greedy (densidad)
+def evaluate_solution(solution):
+    return functools.reduce(lambda x, value: x + value.value, solution, 0)
+
+#Greedy -> Los artículos de mayor valor primero
 def greedy_algorithm(things, capacity):
-    sorted_things = {k: v for k,v in sorted(things.items(), key=lambda item: item[1][1], reverse=True)}
+    sorted_things = sorted(things, key=lambda item: item.value, reverse=True)
+    
+    value = 0
+    weight =0
+    taken = [0]*len(things)
+    
 
-    knapsack = {}
+    for thing in sorted_things:
+        if thing.weight <= capacity:
+            taken[thing.index] = 1
+            value += thing.value
+            capacity -= thing.weight
 
-    for thing in sorted_things.items():
-        if thing[1][0] <= capacity:
-            knapsack.update({thing[0]: thing[1]})
-            capacity -= thing[1][0]
+    output_data = str(value) + '\n'
+    output_data += ' '.join(map(str, taken))
+    return output_data
 
-    return {'solution': knapsack, 'value': evaluate_solution(knapsack)}
-import itertools
-import functools
+#Density greedy -> Artículos de mayor densidad primero
 
-#Fuerza bruta
+#density_greedy = functools.partial(greedy_algorithm, things=item.value/item.weight, capacity)
 
+def density_greedy(things, capacity):
+    sorted_things = sorted(things, key=lambda item: item.value/item.weight, reverse=True)
+    
+    value = 0
+    weight =0
+    taken = [0]*len(things)
+    
+
+    for thing in sorted_things:
+        if thing.weight <= capacity:
+            taken[thing.index] = 1
+            value += thing.value
+            capacity -= thing.weight
+
+    output_data = str(value) + '\n'
+    output_data += ' '.join(map(str, taken))
+    return output_data
+
+#Programación dinámica
+
+def knapsack_td(items, capacity):
+    # """ Resuelve el problema del knapsack utilizando recursividad  de con memoizacion.
+    #  things es una lista de namedtuples de la forma (name, weight, value)"""
+    taken = [0]*len(items)
+    @functools.lru_cache(maxsize=None)
+    def best_value(i, capacity):
+        # Return the value of the most valuable subsequence of the first
+        # i elements in items whose weights sum to no more than capacity.
+        if capacity < 0:
+            return float('-inf')
+        if i == 0:
+            return 0
+        _, weight, value  = items[i - 1]
+        return max(best_value(i - 1, capacity), best_value(i - 1, capacity - weight) + value)
+
+    j = capacity
+    
+    for i in reversed(range(len(items))):
+        if best_value(i + 1, j) != best_value(i, j):
+            j -= items[i][1]
+            taken[i]=1
+            
+    output_data = str(best_value(len(items), capacity)) + '\n'
+    output_data += ' '.join(map(str, taken))
+
+    return output_data
 
 ## TODO: Modifica este diccionario
-algorithms = {'trivial_greedy': trivial_greedy, 'greedy_algorithm': greedy_algorithm}
+algorithms = {'trivial_greedy': trivial_greedy, 'knapsack_td':knapsack_td,
+              'greedy_algorithm': greedy_algorithm, 'density_greedy': density_greedy}
 
 def solve_it(input_data, algorithm=trivial_greedy):
     # Modify this code to run your optimization algorithm
